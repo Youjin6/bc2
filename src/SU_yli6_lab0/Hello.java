f7c640dc18be081eb7832edbeb49625069f4b2b2
Youjin <leeyucong@yahoo.com>
2021-02-08 23:25:00 -0800

    0

---
+++ src/SU_yli6_lab0/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * Yucong L
+ * CPSC 5002, Seattle University
+ * This is free and unencumbered software released into the public domain.
+ */
+package SU_yli6_lab0;
+
+/**
+ * This is a sample program that will print "Hello, World!" to screen.
+ *
+ * @author Yucong L
+ * @version 1.0
+ */
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }
+}
