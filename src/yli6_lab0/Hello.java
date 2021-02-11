117347a052202bc90e863a9faa2b34b38ab5a44c
Youjin <leeyucong@yahoo.com>
2021-02-10 21:00:46 -0800

    0

--- src/yli6_lab0/Hello.java
+++ src/yli6_lab0/Hello.java
@@ -3,7 +3,7 @@
  * CPSC 5002, Seattle University
  * This is free and unencumbered software released into the public domain.
  */
-package SU_yli6_lab0;
+package yli6_lab0;
 
 /**
  * This is a sample program that will print "Hello, World!" to screen.
