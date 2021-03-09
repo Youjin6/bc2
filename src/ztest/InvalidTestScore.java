534fef140e1d6301c87761c57b49cd59f32a43ec
Youjin <leeyucong@yahoo.com>
2021-03-08 20:11:34 -0800

    0

---
+++ src/ztest/InvalidTestScore.java
@@ -0,0 +1,11 @@
+package ztest;
+
+
+public class InvalidTestScore extends Exception {
+    InvalidTestScore(){
+//        System.out.println("enter a number between 0-100");
+    super("Error: .....");
+    }
+
+
+}
